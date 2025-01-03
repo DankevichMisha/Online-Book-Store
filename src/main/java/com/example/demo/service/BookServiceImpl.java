d5b62c84dbc395d2fca9478194539369db743bda
Mihail <mishadyakivnich@gmail.com>
2025-01-03 16:27:09 +0200

    part1

--- src/main/java/com/example/demo/service/BookServiceImpl.java
+++ src/main/java/com/example/demo/service/BookServiceImpl.java
@@ -1,4 +1,23 @@
 package com.example.demo.service;
 
-public class BookServiceImpl {
+import com.example.demo.model.Book;
+import com.example.demo.repository.BookRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class BookServiceImpl implements BookService {
+    private final BookRepository bookRepository;
+
+    @Override
+    public Book save(Book book) {
+        return bookRepository.save(book);
+    }
+
+    @Override
+    public List<Book> findAll() {
+        return bookRepository.findAll();
+    }
 }
