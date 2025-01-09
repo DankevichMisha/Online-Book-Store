462a561155c22a0f5379ffaa9ba73027df6e4688
Mihail <mishadyakivnich@gmail.com>
2025-01-09 14:21:24 +0200

    part2

--- src/main/java/com/example/demo/exception/CustomGlobalExceptionHandler.java
+++ src/main/java/com/example/demo/exception/CustomGlobalExceptionHandler.java
@@ -1,7 +1,28 @@
 package com.example.demo.exception;
 
-public class CustomGlobalExceptionHandler extends RuntimeException {
-    public CustomGlobalExceptionHandler(String message) {
-        super(message);
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class CustomGlobalExceptionHandler {
+
+    @ExceptionHandler(DataProcessingException.class)
+    public ResponseEntity<String> handleDataProcessingException(
+            DataProcessingException exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<String> handleEntityNotFoundException(
+            EntityNotFoundException exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(jakarta.persistence.EntityNotFoundException.class)
+    public ResponseEntity<String> handleEntityNotFoundException(
+            jakarta.persistence.EntityNotFoundException exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
     }
 }
